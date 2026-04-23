eaab4a8c24e6ae394da8b5260225440d29de0c3e
RanWeissman <ranw535@gmail.com>
2026-04-23 17:03:42 +0300

    providers.tf fix

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,11 +2,12 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket         = "wedding-tf-state-ran-2026"
-    key            = "terraform.tfstate"
-    region         = "il-central-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
+    bucket                 = "wedding-tf-state-ran-2026"
+    key                    = "terraform.tfstate"
+    region                 = "il-central-1"
+    dynamodb_table         = "terraform-lock"
+    encrypt                = true
+    skip_region_validation = true
   }
 
   required_providers {
